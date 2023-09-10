e6de42e5bc4933f9122b5e4a217e04ac1d3df5c4
victorcngo <hello@victorcango.fr>
2023-09-10 20:59:04 +0200

    test[productions]: add dummy data

--- pages/productions.vue
+++ pages/productions.vue
@@ -1,5 +1,86 @@
+<script setup>
+const sections = [
+  {
+    title: 'Série documentaire',
+    featuredFilm: {
+      title: 'FeaturedFilm Title',
+      url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+    },
+    films: [
+      {
+        title: 'Film 1',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+      {
+        title: 'Film 2',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+      {
+        title: 'Film 3',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+      {
+        title: 'Film 4',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+    ],
+  },
+  {
+    title: 'Documentaire unitaire',
+    featuredFilm: {
+      title: 'FeaturedFilm Title',
+      url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+    },
+    films: [
+      {
+        title: 'Film 1',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+      {
+        title: 'Film 2',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+      {
+        title: 'Film 3',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+      {
+        title: 'Film 4',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+    ],
+  },
+  {
+    title: 'Documentaire corporate',
+    featuredFilm: {
+      title: 'FeaturedFilm Title',
+      url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+    },
+    films: [
+      {
+        title: 'Film 1',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+      {
+        title: 'Film 2',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+      {
+        title: 'Film 3',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+      {
+        title: 'Film 4',
+        url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+      },
+    ],
+  },
+]
+</script>
+
 <template>
-  <div>
-    <h1>Productions</h1>
+  <div class="l-productions">
+    <Hero title="Productions" />
+    <ProductionsSection v-for="(section, i) in sections" :section="section" :key="i" />
   </div>
 </template>
