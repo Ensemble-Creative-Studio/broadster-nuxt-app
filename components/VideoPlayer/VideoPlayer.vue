8e782384e7b1ea85a91a617afa6ccbefa03ce586
victorcngo <hello@victorcango.fr>
2023-09-10 20:58:31 +0200

    feat: create VideoPlayer

--- components/VideoPlayer/VideoPlayer.vue
+++ components/VideoPlayer/VideoPlayer.vue
@@ -1,3 +1,301 @@
+<script setup>
+// TODO: handle fullscreen
+// import { useFullscreen } from '@vueuse/core'
+// const { isFullscreen, enter, exit, toggle } = useFullscreen()
+
+const video = {
+  title: 'Video Title',
+  url: 'https://player.vimeo.com/progressive_redirect/playback/847356020/rendition/1080p/file.mp4?loc=external&signature=f4a64de90b8b76b1fc7c07b9518235626e948b27bbe8e8e869db69e3056cbb1a',
+}
+
+const isVideoPlayerOpen = useState('isVideoPlayerOpen', () => false)
+const isCreditsOpen = ref(false)
+
+// Template refs
+const $video = ref(null)
+const $timeline = ref(null)
+
+const isPlaying = ref(false)
+const isMuted = ref(false)
+const isFullscreen = ref(false)
+
+const videoCurrentTime = ref(0)
+const videoFormatedCurrentTime = ref(formatTime(videoCurrentTime.value))
+const videoDuration = ref(null)
+const videoFormatedDuration = ref(formatTime(videoDuration.value))
+
+let raf = null
+
+function onCloseClick() {
+  isVideoPlayerOpen.value = false
+}
+
+function onToggleCreditsClick() {
+  isCreditsOpen.value = !isCreditsOpen.value
+}
+
+function onTogglePlayClick() {
+  isPlaying.value = !isPlaying.value
+  raf = window.requestAnimationFrame(onUpdate)
+}
+
+function onMuteClick() {
+  isMuted.value = !isMuted.value
+}
+
+function onFullscreenClick() {
+  isFullscreen.value = !isFullscreen.value
+}
+
+function onUpdate() {
+  videoCurrentTime.value = $video?.value?.currentTime
+  raf = window.requestAnimationFrame(onUpdate)
+}
+
+function onVideoTimeUpdate() {
+  videoFormatedCurrentTime.value = formatTime($video?.value?.currentTime)
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60)
+  const seconds = Math.floor(time % 60)
+  const formatedSeconds = seconds < 10 ? `0${seconds}` : seconds
+
+  return `${minutes}:${formatedSeconds}`
+}
+
+function onTimelineClick(event) {
+  seekOnTimeline(event.clientX)
+}
+
+function seekOnTimeline(userX) {
+  const timelineLeft = $timeline.value.getBoundingClientRect().left
+  const timelineWidth = $timeline.value.getBoundingClientRect().width
+  const userXRelativeToTimeline = userX - timelineLeft
+
+  const seekTime = (userXRelativeToTimeline / timelineWidth) * videoDuration.value
+
+  $video.value.currentTime = seekTime
+}
+
+function onVideoEnded() {
+  isPlaying.value = false
+}
+
+function onVideoLoadedData() {
+  videoDuration.value = $video.value.duration
+  videoFormatedDuration.value = formatTime(videoDuration.value)
+}
+
+watch(
+  () => isPlaying.value,
+  (isPlaying) => {
+    if (isPlaying) {
+      $video.value.play()
+    } else {
+      $video.value.pause()
+    }
+  }
+)
+
+watch(
+  () => isMuted.value,
+  (value) => {
+    $video.value.muted = value
+  }
+)
+
+// watch(
+//   (() => isFullscreen.value,
+//   (value) => {
+//     if (value) {
+
+//     } else {
+
+//     }
+//   })
+// )
+
+onBeforeUnmount(() => {
+  window.cancelAnimationFrame(raf)
+  $video.value = null
+})
+</script>
+
 <template>
-  <div></div>
+  <div
+    class="c-video-player"
+    :class="{
+      '-is-open': isVideoPlayerOpen,
+    }"
+  >
+    <VideoPlayerCredits v-show="isCreditsOpen" />
+    <button
+      class="c-video-player-credits-button o-button -has-dark-grey-background"
+      @click="onToggleCreditsClick"
+    >
+      Credits
+    </button>
+    <button
+      v-show="!isCreditsOpen"
+      class="c-video-player-close-button o-button -has-white-outline"
+      @click="onCloseClick"
+    >
+      Close
+    </button>
+    <div class="c-video-player__thumbnail"></div>
+    <video
+      ref="$video"
+      :src="video.url"
+      class="c-video-player__video"
+      playsinline
+      @loadeddata="onVideoLoadedData"
+      @timeupdate="onVideoTimeUpdate"
+      @ended="onVideoEnded"
+    ></video>
+    <footer class="c-video-player-controls">
+      <div class="c-video-player-controls__buttons">
+        <button class="o-button -has-dark-grey-background" @click="onTogglePlayClick">
+          <span v-html="isPlaying ? 'Pause' : 'Play'"></span>
+        </button>
+        <button :class="['o-button -has-dark-grey-background']" @click="onMuteClick">
+          <span v-html="isMuted ? 'Unmute' : 'Mute'"></span>
+        </button>
+        <button :class="['o-button -has-dark-grey-background']" @click="onFullscreenClick">
+          <span v-html="isFullscreen ? 'Exit' : 'Fullscreen'"></span>
+        </button>
+      </div>
+      <div class="c-video-player-controls__time">
+        <div class="c-video-player-controls__duration">
+          <span class="c-video-player-controls__timestamp -is-current">
+            {{ videoFormatedCurrentTime }}
+          </span>
+          <span class="c-video-player-controls__duration -is-total">
+            {{ videoFormatedDuration }}
+          </span>
+        </div>
+        <div
+          ref="$timeline"
+          class="c-video-player-controls__timeline"
+          @click="onTimelineClick"
+          @mousedown="onTimelineMousedown"
+          @mouseup="onTimelineMouseup"
+          @touchstart="onTimelineTouchstart"
+        >
+          <div class="c-video-player-controls__background"></div>
+          <div
+            class="c-video-player-controls__current"
+            :style="{
+              transform: `translate3d(-${100 - (videoCurrentTime / videoDuration) * 100}%, 0, 0)`,
+            }"
+          ></div>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.c-video-player {
+  pointer-events: none;
+  height: 100vh;
+  width: 100%;
+  position: fixed;
+  background-color: $black;
+  z-index: 11;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  visibility: hidden;
+  transition: opacity 0.3s ease-in-out, visibility 0.3s ease-in-out;
+  &.-is-open {
+    opacity: 1;
+    visibility: visible;
+    pointer-events: all;
+  }
+  &-credits-button,
+  &-close-button {
+    position: absolute;
+    top: 2.4rem;
+    z-index: 10;
+  }
+  &-credits-button {
+    left: 2.4rem;
+  }
+  &-close-button {
+    right: 2.4rem;
+  }
+  &__thumbnail,
+  &__video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    height: 100%;
+  }
+  &__thumbnail {
+    z-index: -1;
+  }
+  &__video {
+    z-index: -2;
+  }
+  &-controls {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    padding: 2.4rem;
+    display: flex;
+    align-items: center;
+    &__buttons {
+      & > *:not(:last-child) {
+        margin-right: 1.2rem;
+      }
+    }
+    &__time {
+      flex: auto;
+      margin-left: 1.2rem;
+      position: relative;
+    }
+    &__duration {
+      font-size: 1.2rem;
+      color: $white;
+      margin-bottom: 2.1rem;
+      &.-is-total {
+        color: $medium-grey;
+        margin-left: 0.6rem;
+      }
+    }
+    &__timeline {
+      overflow: hidden;
+      width: 100%;
+      height: 2rem;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+      position: absolute;
+      top: 50%;
+      transform: translateY(calc(-50% + 2rem));
+    }
+    &__background {
+      position: absolute;
+      left: 0;
+      height: 0.1rem;
+      background-color: $medium-grey;
+      opacity: 0.8;
+      width: 100%;
+    }
+
+    &__current {
+      height: 0.1rem;
+      background-color: $white;
+      position: absolute;
+      left: 0;
+      width: 100%;
+      transform: scaleX(0);
+      transform-origin: left;
+    }
+  }
+}
+</style>
---
+++ components/VideoPlayer/VideoPlayerCredits/VideoPlayerCredits.vue
@@ -0,0 +1,67 @@
+<script setup></script>
+
+<template>
+  <div class="c-video-player-credits">
+    <div class="c-video-player-credits__container u-wrapper">
+      <div class="c-video-player-credits__grid">
+        <h2 class="c-video-player-credits__title o-thumbnail-title">
+          Drôles de Belges, font-ils mieux l'humour que les autres ?
+        </h2>
+        <p class="c-video-player-credits__description">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis veniam temporibus omnis
+          a minus reprehenderit explicabo enim nulla delectus quod.
+        </p>
+        <ul class="c-video-player-credits__details">
+          <li class="c-video-player-credits__item">
+            <span class="o-button -has-white-outline">Test</span>
+            <p class="c-video-player-credits__text">France / Belgique et Suisse</p>
+          </li>
+          <li class="c-video-player-credits__item">
+            <span class="o-button -has-white-outline">Test</span>
+            <p class="c-video-player-credits__text">France / Belgique et Suisse</p>
+          </li>
+          <li class="c-video-player-credits__item">
+            <span class="o-button -has-white-outline">Test</span>
+            <p class="c-video-player-credits__text">France / Belgique et Suisse</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.c-video-player-credits {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 9;
+  padding-top: 20rem;
+  background-color: rgba($black, 0.8);
+  color: $white;
+  &__grid {
+    @include grid(12, 1.2rem, 1.2rem);
+  }
+  &__title,
+  &__description,
+  &__details {
+    grid-column: 1 / span 4;
+  }
+  &__description,
+  &__details {
+    margin-top: 3.7rem;
+  }
+  &__item {
+    display: flex;
+    align-items: center;
+    &:not(:last-child) {
+      margin-bottom: 1.8rem;
+    }
+  }
+  &__text {
+    margin-left: 1.8rem;
+  }
+}
+</style>
