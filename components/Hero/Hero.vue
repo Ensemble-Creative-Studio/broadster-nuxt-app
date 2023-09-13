bf86ac75eacf59988dc7988d78acb9084b0bb5c4
victorcngo <hello@victorcango.fr>
2023-09-13 22:55:03 +0200

    feat[Hero]: create different formats

--- components/Hero/Hero.vue
+++ components/Hero/Hero.vue
@@ -1,17 +1,26 @@
 <script setup>
+const route = useRoute()
+
 const props = defineProps({
   title: {
     type: String,
     required: true,
   },
   video: {
-    type: String
+    type: String,
   },
 })
 </script>
 
 <template>
-  <div class="c-hero">
+  <div
+    class="c-hero"
+    :class="{
+      '-is-productions': route.name === 'productions',
+      '-is-services': route.name === 'services',
+      '-is-infos': route.name === 'infos',
+    }"
+  >
     <h1 class="c-hero__title o-section-title">{{ title }}</h1>
     <div v-if="video" class="c-hero-video">
       <video :src="video" class="c-hero-video__source" autoplay muted loop playsinline></video>
@@ -24,6 +33,7 @@ const props = defineProps({
   height: 100svh;
   width: 100%;
   position: relative;
+  $self: &;
   @include mq($until: tablet) {
     height: 75svh;
   }
@@ -41,9 +51,17 @@ const props = defineProps({
     z-index: -1;
     border-radius: 0.4rem;
     overflow: hidden;
-    aspect-ratio: 1 / 1;
     height: min(60vw, 40rem);
     width: auto;
+    #{$self}.-is-productions & {
+      aspect-ratio: 16 / 9;
+    }
+    #{$self}.-is-services & {
+      aspect-ratio: 4 / 3;
+    }
+    #{$self}.-is-infos & {
+      aspect-ratio: 9 / 16;
+    }
   }
 }
 </style>
