d1ea85a3b15111a99167eb1e272a99e370d229ec
victorcngo <hello@victorcango.fr>
2023-10-02 20:18:31 +0200

    fix: update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  devtools: { enabled: false },
+  // devtools: { enabled: false },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'initial-scale=1',
+      title: 'Broadster',
+      meta: [
+        { name: 'description', content: 'Broadster' },
+        { name: 'color-scheme', content: 'black' }, // Add this line
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/favicons/apple-touch-icon.png',
+        },
+        { rel: 'icon', sizes: '150x150', href: '/favicon/favicon-150x150.png' },
+        { rel: 'icon', sizes: '144x144', href: '/favicon/favicon-144x144.png' },
+        { rel: 'icon', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'icon', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+        { hid: 'icon', rel: 'icon', href: '/favicon/favicon.svg' },
+        {
+          hid: 'mask-icon',
+          rel: 'mask-icon',
+          href: '/favicon/safari-pinned-tab.svg',
+          color: '#000000',
+        },
+        {
+          hid: 'apple-touch-icon',
+          rel: 'apple-touch-icon',
+          href: '/favicon/apple-touch-icon.png',
+        },
+        {
+          hid: 'manifest',
+          rel: 'manifest',
+          href: '/favicon/manifest.json',
+          crossorigin: 'use-credentials',
+        },
+      ],
+    },
+  },
   modules: ['@nuxtjs/sanity', '@vueuse/nuxt', 'nuxt-lazy-load'],
   lazyLoad: {
     // These are the default values
